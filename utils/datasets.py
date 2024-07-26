1608a8872c56ce848173776f7d75e1da07688536
Peter Kruse <pkruser@vols.utk.edu>
2024-07-26 11:02:35 -0400

    changed naming convention, added functions

--- utils/datasets.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class semantic_scholar:
-    dataset: str = "allenai/peS2o"
-    file: str = "examples/get_semantic_scholar.py"
-    train_split: str = "train"
-    test_split: str = "validation"

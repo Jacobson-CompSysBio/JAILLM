0ab01c0f93f9bf959490179c0663f6389efa0836
pikruse <pkruse@vols.utk.edu>
2024-09-09 11:47:47 -0400

    added assertion error preproc code

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -62,9 +62,12 @@ def format_chat(row,
         row (dict): dictionary containing the chat template
     """
 
-    row_json = [{'role': 'user', 'content': row[input_col]},
-                {'role': 'assistant', 'content': row[output_col]}]
-    row["text"] = pipeline_name.tokenizer.apply_chat_template(row_json, tokenize=False)
+    row_input = [{'role': 'user', 'content': row[input_col]}]
+    row_output = [{'role': 'assistant', 'content': row[output_col]}]
+
+    row["input"] = pipeline_name.tokenizer.apply_chat_template(row_input, tokenize=False)
+    row["output"] = pipeline_name.tokenizer.apply_chat_template(row_output, tokenize=False)
+
     return row
 
 def format_network_chat(row,
@@ -87,13 +90,14 @@ def format_network_chat(row,
     """
 
     row_json = [{'role': 'user',
-                 'content': 'In and undirected weighted graph, (i,j) means that node i and node j are connected with an undirected, weighted edge. The nodes are: {} and the edges are: {}\n Is there a cycle in this graph?'},
+                 'content': 'In and undirected weighted graph, (i,j) means that node i and node j are connected with an undirected, weighted edge. The nodes are: {nodes} and the edges are: {edges}.\n {question}'},
                 {'role': 'assistant', 'content': 'yes'}]
     row["text"] = pipeline_name.tokenizer.apply_chat_template(row_json, tokenize=False)
     return row
 
 def tokenize_data(examples,
-                  pipeline_name):
+                  pipeline_name,
+                  type: str = 'nextchar'):
     
     """
     Function to preprocess the data for training on next-character prediction
@@ -105,21 +109,17 @@ def tokenize_data(examples,
     Returns:
         tokenized_data (dict): dictionary containing the tokenized data
     """
-
-    tokenized_data = pipeline_name.tokenizer(text=examples['text'],
+    assert type in ['nextchar', 'chat'], "type must be either 'nextchar' or 'chat'"
+    
+    if type == 'nextchar':
+        tokenized_data = pipeline_name.tokenizer(text=examples['text'],
                                padding='max_length', 
                                truncation=True, 
                                max_length=1024)
-    
-    labels = tokenized_data['input_ids'].copy()
-    
-    for i in range(len(labels)):
-        if labels[i][-1] != pipeline_name.tokenizer.pad_token_id:
-            labels[i] = labels[i][1:] + [pipeline_name.tokenizer.pad_token_id]
-        else:
-            labels[i] = labels[i][1:] + [pipeline_name.tokenizer.pad_token_id]
-
-    labels = [[pipeline_name.tokenizer.pad_token_id if x == pipeline_name.tokenizer.pad_token_id else x for x in y] for y in labels]
-    tokenized_data['labels'] = labels
-    
+    elif type == 'chat':
+        tokenized_data= pipeline_name.tokenizer(text=examples['input'],
+                                                text_target=examples['output'],
+                                                padding='max_length', 
+                                                truncation=True, 
+                                                max_length=1024)
     return tokenized_data
